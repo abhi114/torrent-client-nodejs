811716ae4d0f4705b1ef28ee06a3a093a0afe5d0
abhi114 <nabhi531@gmail.com>
2024-09-13 14:46:47 +0530

    bencode added

--- index.js
+++ index.js
@@ -5,6 +5,16 @@
 ///With strict mode, you can not, for example, use undeclared variables.
 //Converting a Buffer into a string using one of the above is referred to as decoding,
 // and converting a string into a Buffer is referred to as encoding.
+//In a torrent file, the announce property specifies the URL of the tracker that coordinates the 
+//distribution of the torrent's content. A tracker is a server that keeps track of which peers have which pieces of the torrent and helps peers find each other to exchange data.
+
+//In other words, the announce property contains the URL that a BitTorrent client would use to announce its
+//presence to the tracker and retrieve a list of other peers that are sharing the same torrent.
+
+//For example, the announce property might contain a URL like 
+//http://example.com:6969/announce, which the BitTorrent client would use to connect to the tracker and participate in the torrent's distribution.
 const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
-console.log(torrent.toString('utf8'))
+const bencode = require("bencode");
+const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+console.log(torrent.announce.toString("utf8"));
+
