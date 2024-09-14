7a97a2a5b9b25a33e6db316c38d279d608a2a618
abhi114 <nabhi531@gmail.com>
2024-09-14 18:09:08 +0530

    index updated

--- index.js
+++ index.js
@@ -16,7 +16,8 @@
 const fs = require('fs');
 const bencode = require("bencode");
 const tracker = require('./tracker');
-const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+const torrentParser = require('./torrent-parser');
+const torrent = torrentParser.open('puppy.torrent');
 tracker.getPeers(torrent,peers=>{
     console.log("list of peers:",peers);
 })
---
+++ torrent-parser.js
@@ -0,0 +1,15 @@
+'use strict';
+const fs = require('fs');
+const bencode = require('bencode');
+
+module.exports.open = (filepath) =>{
+    return bencode.decode(fs.readFileSync(filepath));
+};
+
+module.exports.size = torrent =>{
+    //
+}
+
+module.exports.infoHash = torrent=>{
+    //
+}
