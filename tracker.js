9b2373f2154dee37b1bc235f71c697b7cd44086a
abhi114 <nabhi531@gmail.com>
2024-09-14 17:18:06 +0530

    announce complete

--- tracker.js
+++ tracker.js
@@ -4,6 +4,8 @@ const dgram = require('dgram');
 const Buffer = require('buffer');
 const urlParse = require('url').parse;
 const crypto = require('crypto'); //for creating a random buffer for the transaction_id
+const torrentParser = require('./torrent-parser');
+const util = require('./util');
 
 module.exports.getPeers = (torrent,callback) =>{
     const socket = dgram.createSocket('udp4');
@@ -59,7 +61,38 @@ function parseConnResp(resp){
     connectionId:resp.slice(8),
   }
 }
-function buildAnnounceReq(connId){
+function buildAnnounceReq(connId,torrent,port=6881){
+  //totaal bits of the req will be 98 and the port will be 6881 only
+  const buf = Buffer.allocUnsafe(98);
+  //connection id
+  connId.copy(buf, 0);
+  //action
+  buf.writeUInt32BE(1, 8);
+  //transaction id
+  crypto.randomBytes(4).copy(buf, 12);
+  //info hash
+  torrentParser.infoHash(torrent).copy(buf, 16); //it is a 20 byte(not bit rest are bit) string so next will be at index 36
+  //peer id
+  util.genId().copy(buf, 36);
+  //downloaded
+  //I need a 64-bit integer but since they’re always initialized to 0, 
+  //I just used Buffer.alloc(8), as this will just create an 8-byte buffer with all 0s.
+  Buffer.alloc(8).copy(buf,56); //8byte buffer which will be equal to 64 bit 
+  //left
+  torrentParser.size(torrent).copy(buf,64);
+  //uploaded
+  Buffer.alloc(8).copy(buf,72);
+  //event
+  buf.writeUInt32BE(0,80);
+  //ip address
+  buf.writeUInt32BE(0,84);
+  //key
+  crypto.randomBytes(4).copy(buf,88);
+  //num want
+  buf.writeInt32BE(-1,92);
+  //port
+  buf.writeUInt16BE(port,96);
+  return buf;
 
 }
 function parseAnnounceResp(resp){
